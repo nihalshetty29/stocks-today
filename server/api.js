3315208cd985a30abab01cc327c9f958962f2824
nihalshetty29 <nihalshetty29@gmail.com>
2023-09-22 16:10:38 +0530

    Add aggregates and financials endpoints

--- server/api.js
+++ server/api.js
@@ -3,7 +3,7 @@ const axios = require("axios");
 const api = axios.create({
     baseURL: "https://api.polygon.io",
     headers: {
-        Authorization: `Bearer wJtsJ4jukIe0BZomBt8ODFHVffk6l_4o`,
+        Authorization: `Bearer ${process.env.API_TOKEN}`,
     },
 });
 
--- server/app.js
+++ server/app.js
@@ -20,6 +20,7 @@ const func = () => {
 
 const express = require("express");
 const cors = require("cors");
+require("dotenv").config();
 
 const api = require("./api");
 
@@ -34,6 +35,23 @@ const getYesterdayDate = () => {
 
     const formattedDate = `${year}-${month}-${day}`;
     return formattedDate;
+};
+
+const getLast30DaysDates = () => {
+    const currentDate = new Date(); // Get the current date
+    currentDate.setDate(currentDate.getDate() - 1); // Set the date to yesterday
+    currentDate.setHours(0, 0, 0, 0); // Set the time to 00:00:00:000
+
+    const endDate = currentDate.toISOString().split("T")[0]; // Format as "YYYY-MM-DD"
+
+    currentDate.setDate(currentDate.getDate() - 29);
+
+    const startDate = currentDate.toISOString().split("T")[0]; // Format as "YYYY-MM-DD"
+
+    return {
+        startDate,
+        endDate,
+    };
 };
 
 app.use(cors());
@@ -66,6 +84,33 @@ app.get("/grouped-daily", async (req, res) => {
     try {
         const { data } = await api.get(
             `/v2/aggs/grouped/locale/us/market/stocks/${date}`
+        );
+        res.status(200).json({ message: "Data fetched successfully", data });
+    } catch (err) {
+        res.status(404).json({ message: "An error occured", err });
+    }
+});
+
+app.get("/aggregates", async (req, res) => {
+    const { ticker } = req.query;
+    const { startDate, endDate } = getLast30DaysDates();
+
+    try {
+        const { data } = await api.get(
+            `/v2/aggs/ticker/${ticker}/range/1/day/${startDate}/${endDate}`
+        );
+        res.status(200).json({ message: "Data fetched successfully", data });
+    } catch (err) {
+        res.status(404).json({ message: "An error occured", err });
+    }
+});
+
+app.get("/financials", async (req, res) => {
+    const { ticker } = req.query;
+
+    try {
+        const { data } = await api.get(
+            `/vX/reference/financials?ticker=${ticker}&timeframe=quarterly`
         );
         res.status(200).json({ message: "Data fetched successfully", data });
     } catch (err) {
